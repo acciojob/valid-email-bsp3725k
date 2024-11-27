12b5d9e1313388054d6bec28615b0f14cca8c9a1
bsp3725k <139534037+bsp3725k@users.noreply.github.com>
2024-11-27 14:03:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function validEmail(str) {
   //your JS code here.
-}
+  const emailRegex = /^[A-Za-z0-9]+(?:[.-][A-Za-z0-9]+)*@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
 
+  // Test the email string against the regular expression
+  return emailRegex.test(str);}
 // Do not change the code below.
 const str = prompt("Enter an email address.");
 alert(validEmail(str));
